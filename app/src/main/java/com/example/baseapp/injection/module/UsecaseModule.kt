adda336c5417d32ee061770692467e22bed1db21
RISHVIK VARDHAN <45693730+S-Rishvik@users.noreply.github.com>
2021-03-24 11:37:03 +0530

    Saurabh Requested Changes

--- app/src/main/java/com/example/baseapp/injection/module/UsecaseModule.kt
+++ app/src/main/java/com/example/baseapp/injection/module/UsecaseModule.kt
@@ -1,32 +1,27 @@
 package com.example.baseapp.injection.module
 
-import com.example.baseapp.injection.scope.ActivityScope
 import com.example.domain.repository.IAuthRepo
 import com.example.domain.usecase.auth.IsUserLoggedInUseCase
 import com.example.domain.usecase.auth.LoginUserUseCase
 import com.example.domain.usecase.auth.LogoutUserUseCase
 import dagger.Module
 import dagger.Provides
-import javax.inject.Singleton
 
 @Module
-class UsecaseModule {
+class UseCaseModule {
 
   @Provides
-  @ActivityScope
-  fun provideLoginUserUseCase(authRepo: IAuthRepo) : LoginUserUseCase{
+  fun provideLoginUserUseCase(authRepo: IAuthRepo): LoginUserUseCase {
     return LoginUserUseCase(authRepo)
   }
 
   @Provides
-  @ActivityScope
-  fun provideLogoutUserUseCase(authRepo: IAuthRepo) : LogoutUserUseCase{
+  fun provideLogoutUserUseCase(authRepo: IAuthRepo): LogoutUserUseCase {
     return LogoutUserUseCase(authRepo)
   }
 
   @Provides
-  @ActivityScope
-  fun provideIsUserLoggedInUseCase(authRepo: IAuthRepo) : IsUserLoggedInUseCase{
+  fun provideIsUserLoggedInUseCase(authRepo: IAuthRepo): IsUserLoggedInUseCase {
     return IsUserLoggedInUseCase(authRepo)
   }
 }
